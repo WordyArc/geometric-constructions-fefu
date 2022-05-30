790d7f92e56a25e90621b07dffe8d10ee14135d1
WordyArc <vandal.vector@outlook.com>
2022-05-30 23:12:39 +1000

    add Footer component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Navibar from "./components/Navibar/Navibar";
+import Footer from "./components/Footer/Footer";
 
 
 function App() {
   return (
     <div className="App">
       <Navibar/>
+      <Footer/>
     </div>
   );
 }
