852f29636cca22aecfa14937bbccfda73a47e3ec
WordyArc <vandal.vector@outlook.com>
2022-05-31 19:14:30 +1000

    add About component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Navibar from "./components/Navibar/Navibar";
 import Footer from "./components/Footer/Footer";
-// import Info from "./components/Info/Info";
+/*import About from "./components/About/About";
+import Info from "./components/Info/Info";*/
+
 
 function App() {
   return (
     <div className="App">
       <div className="wrapper d-flex flex-column min-vh-100">
           <Navibar/>
-          {/*<Info/>*/}
+          {/*<About/>
+          <Info/>*/}
           <Footer/>
       </div>
 
