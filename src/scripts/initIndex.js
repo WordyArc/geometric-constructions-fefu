6c9bbd4eca9622f7a4f2f0edb4fff7fcbe7373a6
viktor <ashenarx@gmail.com>
2022-06-01 00:45:29 +1000

    fix adaptive GraphicEditor

--- src/scripts/initIndex.js
+++ src/scripts/initIndex.js
@@ -1,6 +1,6 @@
 var params = {
-    "appName": "geometry", "width": 1200, "height": 700, "showToolBar": true, "showAlgebraInput": true, "showMenuBar": true,
-    "scaleContainerClass": "ggb-container", "allowUpscale": true
+    "appName": "geometry", "width": 1000, "height": 700, "showToolBar": true, "showAlgebraInput": true, "showMenuBar": true,
+    "scaleContainerClass": "ggb-container", "allowUpscale": true, "language": "russian"
 };
 
 var ggbApplet = new window.GGBApplet(params, true);
