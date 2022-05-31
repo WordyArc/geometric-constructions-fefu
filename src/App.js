f57740a73a0f515da158a4227b10ae4ee2d79158
viktor <ashenarx@gmail.com>
2022-06-01 00:32:23 +1000

    add GraphicEditor component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Navibar from "./components/Navibar/Navibar";
 import Footer from "./components/Footer/Footer";
+import GraphicEditor from "./components/GraphicEditor/GraphicEditor";
 /*import About from "./components/About/About";
 import Info from "./components/Info/Info";*/
-
 
 function App() {
   return (
     <div className="App">
       <div className="wrapper d-flex flex-column min-vh-100">
           <Navibar/>
+          <GraphicEditor/>
           {/*<About/>
           <Info/>*/}
           <Footer/>
