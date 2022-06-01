196e18ec7693a242479a2cea01cca5db21344f28
viktor <ashenarx@gmail.com>
2022-06-02 02:09:17 +1000

    refactoring initGGBApp script

--- src/components/GraphicEditor/GraphicEditor.jsx
+++ src/components/GraphicEditor/GraphicEditor.jsx
@@ -1,28 +1,25 @@
 import React from 'react';
 import {Container} from "react-bootstrap";
 // import Geogebra from "react-geogebra";
-import '../../scripts/initIndex'
+import '../../scripts/initGGBApp'
 import './GraphicEditor.css'
+import Geogebra from "../../scripts/initGGBApp";
 
 
 const GraphicEditor = () => {
     return (
 
             <Container className="mt-5 d-flex flex-column justify-content-center align-items-center" id="main-container">
-                {/*<Geogebra
-                    width="800"
-                    height="600"
-                    appName="geometry"
-                    scaleContainerClass="ggb-container"
-                    showMenuBar
-                    showToolBar
-                    showAlgebraInput
-                />*/}
-
                 <h1 className="text-center pt-3">Редактор</h1>
-                <div className="ggb-container col-12 col-lg-auto">
+
+                {/*<div className="ggb-container col-12 col-lg-auto">
                     <div id="ggb-element"></div>
+                </div>*/}
+
+                <div className="col-12 col-lg-auto">
+                    <Geogebra/>
                 </div>
+
                 <hr/>
                 <p>На данной странице вы можете выполнять построения в пространстве с помощью GeoGebra.</p>
             </Container>
