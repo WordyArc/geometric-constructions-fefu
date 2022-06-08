d73674aeea56fbfcc57262ab68291faed1b2e33a
viktor <ashenarx@gmail.com>
2022-06-08 21:54:53 +1000

    update CreateTask

--- src/components/CreateTask/CreateTask.jsx
+++ src/components/CreateTask/CreateTask.jsx
@@ -3,8 +3,11 @@ import React, {useEffect, useState} from 'react';
 import {db} from '../../firebase-config'
 import {addDoc, collection, getDocs, serverTimestamp} from "firebase/firestore";
 import Geogebra from "../../scripts/InitGGBApp";
+import {Button} from "react-bootstrap";
+import './CreateTask.css'
 
 const CreateTask = () => {
+
     function ChangeMode() {
         const app = window.appId;
         if (document.getElementById('edit-mode').innerText === "Режим презентации") {
@@ -64,12 +67,6 @@ const CreateTask = () => {
         else return;
     }
 
-    function saveGgbFile(){
-        const app = window.appId;
-        return app.getBase64()
-    }
-
-
     const [tasks, setTasks] = useState([]);
     const tasksCollectionRef = collection(db, 'tasks')
     useEffect(() => {
@@ -89,37 +86,54 @@ const CreateTask = () => {
     const [newType, setNewType] = useState("")
     const newCreatedAT = serverTimestamp();
 
-
     useEffect(() => {
-
-
-
-        // setNewScenes(scenesObj)
-
+        const createTask = async () => {
+            const app = await window.appId;
+            await setNewBase64(app.getBase64);
+        }
+        createTask()
     })
 
-    function createTask2 () {
-        const app = window.appId;
-        if (newBase64 === "") {
-            let e = setNewBase64(app.getBase64)
-            setTimeout(e, 1000)
-        }
+    const setTask = async () => {
         const strScene = JSON.stringify(scenes, replacer);
         setNewScenes(strScene);
-        const createTask = async () => {
+        if (strScene !== "") {
             await addDoc(tasksCollectionRef, {
                 title: newTitle,
                 description: newDescription,
                 solution: newSolution,
                 base64: newBase64,
-                scenes: newScenes,
+                scenes: strScene,
                 type: newType,
                 createdAt: newCreatedAT
             });
+            alert('Задача успешно создана')
+        } else {
+            alert('Создание задачи прервалось, пожалуйста нажмите "Создать задачу" снова')
         }
-        createTask()
     }
 
+
+    let arr = [];
+    let startScene = 0;
+    let key = 1;
+    function setScene() {
+        const app = window.appId;
+        let i, strName;
+        let objNumber = app.getObjectNumber();
+        for (i = startScene; i < objNumber; i++) {
+            strName = app.getObjectName(i);
+            arr[i] = strName;
+            startScene = objNumber;
+            // console.log(objNumber);
+            if (i === objNumber-1) {
+                scenes.set(key, arr);
+                key++;
+            }
+        }
+        console.log(arr);
+        arr = [];
+    }
 
 
     function replacer(key, value) {
@@ -132,6 +146,36 @@ const CreateTask = () => {
             return value;
         }
     }
+
+
+
+
+
+    /*function createTask () {
+        const app = window.appId;
+        if (newBase64 === "") {
+            let e = setNewBase64(app.getBase64)
+            setTimeout(e, 1000)
+        }
+        const strScene = JSON.stringify(scenes, replacer);
+        setNewScenes(strScene);
+        const setTask = async () => {
+            await addDoc(tasksCollectionRef, {
+                title: newTitle,
+                description: newDescription,
+                solution: newSolution,
+                base64: newBase64,
+                scenes: newScenes,
+                type: newType,
+                createdAt: newCreatedAT
+            });
+        }
+        setTask()
+    }*/
+
+
+
+
 
 
 
@@ -163,92 +207,82 @@ const CreateTask = () => {
 
 
 
-    let arr = [];
-    let startScene = 0;
-    let key = 1;
-    function setScene() {
-        const app = window.appId;
-        let i, strName;
-
-        let objNumber = app.getObjectNumber();
-        for (i = startScene; i < objNumber; i++) {
-            strName = app.getObjectName(i);
-            // addScene2(sceneNumber, [strName])
-            arr[i] = strName;
-            startScene = objNumber;
-            // console.log(objNumber);
-            if (i === objNumber-1) {
-                scenes.set(key, arr);
-                key++;
-            }
-        }
-        console.log(arr);
-        arr = [];
-
-        // addScene2(key, arr);
-    }
-
-
-    //
-    function Test() {
-        console.log(scenes.size)
-        console.log(scenes.get(sceneNumber))
-        console.log(newBase64)
-
-
-
-
-    }
 
 
 
     return (
-        <div className="container mt-auto">
-            <Geogebra
-                debug
-                id="appId"
-                language="english"
-                appName="3d"
-                width="600"
-                height="400"
-                enableUndoRedo="false"
-                useBrowserForJS="true"
-            />
-            <div className="btn-group d-flex justify-content-center w-50 under-buttons">
-                <button className="btn btn-dark" onClick={prevScene}>Предыдущий рисунок</button>
-                <button className="btn btn-dark" id="current-scene">Рисунок 1</button>
-                <button className="btn btn-dark" onClick={nextScene} id="nextSceneButton">Следующий
-                    рисунок
-                </button>
-            </div>
-            <div className="d-flex justify-content-center under-buttons">
-                <button className="btn btn-dark" id="edit-mode" onClick={ChangeMode}>Режим презентации</button>
-            </div>
-            <input placeholder="Title" type="text" onChange={(event) => {setNewTitle(event.target.value)}}/>
-            <input placeholder="description" type="text" onChange={(event) => {setNewDescription(event.target.value)}}/>
-            <input placeholder="solution" type="text" onChange={(event) => {setNewSolution(event.target.value)}}/>
-            {/*<input placeholder="base64" type="text" onChange={(event) => {setNewBase64(event.target.value)}}/>*/}
-            {/*<input placeholder="scenes" type="text" onChange={(event) => {setNewScenes(event.target.value)}}/>*/}
-            <input placeholder="type" type="text" onChange={(event) => {setNewType(event.target.value)}}/>
-            {/*<button onClick={createTask}>Create task</button>*/}
-            <button onClick={createTask2}>Create task2</button>
-            <button onClick={setScene}>сохранить сцену</button>
-            <button onClick={Test}>Test</button>
-            {/*<button onClick={saveGgbFile}>save</button>*/}
-            {/*{tasks.map((task) => {
-                return (
-                    <div className="mt-5">
-                        {" "}
-                        <h1> ID: {task.id} </h1>
-                        <h1> Title: {task.title} </h1>
-                        <h1> Description: {task.description} </h1>
-                    </div>
-                );
-            })}*/}
-            {/*<div>
-                { userDataArray.map((userData) => userData.id) }
-            </div>*/}
 
+
+        <div className="m-auto mx-lg-5">
+            <div className="task__wrapper container-fluid shadow-lg  bg-light align-content-center py-4" id="main-container">
+                <div className="row d-flex align-items-center">
+                    <div className="col-lg-6">
+                        <div className="d-flex flex-column align-items-center">
+                            <div className="d-flex align-items-center shadow">
+                                <Geogebra
+                                    debug
+                                    id="appId"
+                                    language="english"
+                                    appName="3d"
+                                    width="600"
+                                    height="400"
+                                    enableUndoRedo="false"
+                                    useBrowserForJS="true"
+                                />
+                            </div>
+                            <div className="mt-3 d-flex justify-content-center w-50">
+                                <div className="btn-group">
+                                    <Button className="btn-dark" onClick={prevScene}>Предыдущий рисунок</Button>
+                                    <div className="bg-dark text-primary d-flex align-items-center"
+                                         id="current-scene">Рисунок 1
+                                    </div>
+                                    <Button className="btn-dark" onClick={nextScene} id="nextSceneButton">Следующий рисунок</Button>
+                                </div>
+                                <Button className="mx-2 btn-dark" onClick={setScene}>Сохранить сцену</Button>
+                            </div>
+                            <div className="d-flex justify-content-center under-buttons">
+                                <Button className="btn-dark mt-2" id="edit-mode" onClick={ChangeMode}>Режим презентации</Button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="d-flex flex-column justify-content-between align-items-center col-lg-6 mt-4 mt-lg-0" id="task-text">
+                        <div>
+                            <h4>Название задачи</h4>
+                            <input className="create-task__title-input" placeholder="Введите название задачи..." type="text" onChange={(event) => {setNewTitle(event.target.value)}}/>
+                        </div>
+                        <hr/>
+                        <div>
+                            <h4>Описание задачи</h4>
+                            <textarea className="create-task__description-input" placeholder="Введите описание задачи..." onChange={(event) => {setNewDescription(event.target.value)}}/>
+                        </div>
+                        <hr/>
+                        <div>
+                            <h4>Решение задачи</h4>
+                            <textarea className="create-task__solution-input" placeholder="Введите решение задачи..." onChange={(event) => {setNewSolution(event.target.value)}}/>
+                        </div>
+                        <hr/>
+                        <div>
+                            <h4>Тип задачи</h4>
+                            <div className="d-flex flex-row">
+                                <div className="mx-2">
+                                    <input className="mx-1" id="type1" name="taskType" type="radio" onChange={(event) => {setNewType("Циркуль")}}/>
+                                    <label htmlFor="type1">Циркуль</label>
+                                </div>
+                                <div className="mx-2">
+                                    <input className="mx-1" id="type2" name="taskType" type="radio" onChange={(event) => {setNewType("Линейка")}}/>
+                                    <label htmlFor="type2">Линейка</label>
+                                </div>
+                                <div className="mx-2">
+                                    <input className="mx-1" id="type3" name="taskType" type="radio" checked onChange={(event) => {setNewType("Смешанная")}}/>
+                                    <label htmlFor="type3">Смешанная</label>
+                                </div>
+                            </div>
+                        </div>
+                        <hr/>
+                        <Button className="btn-primary" onClick={setTask}>Создать задачу</Button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
